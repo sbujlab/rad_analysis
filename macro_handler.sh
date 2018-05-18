58d7cc3e63d4076878f90d0b3db6e279eba71c19
Cameron Clarke <cameronc137@gmail.com>
2018-05-18 17:18:35 -0400

    White space in mollerMother.gdml caused segfaults...... who knows why.

--- macro_handler.sh
+++ macro_handler.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #
 
+cd ..
+cd build
+cd ..
+cd build
+cd ..
+cd build
+BUILD=$PWD
+
 ############################ 
 ##### Define Functions #####
 
@@ -9,11 +17,14 @@
 
 iterator () {
 
-  outLocation=$HOME/gitdir/remoll/build/output/
+  outLocation=$PWD/output/
   cd $outLocation
   initial=1
   
-  read -p 'Number of jobs: ' final
+  if [ $changeInclude -eq 1 ];
+    then
+    read -p 'Number of jobs: ' final
+  fi
 
   if [ $changeInclude -eq 0 ];
     then
@@ -30,15 +41,17 @@ iterator () {
     mkdir $name
     cd $name
     mkdir geometry
-    cp -rp "${HOME}"/gitdir/remoll/geometry/*${mod}* "./geometry/"
-    cp -rp "${HOME}"/gitdir/remoll/build/remoll "./remoll"
-    cp -rp "${HOME}"/gitdir/remoll/build/macros/runscript_${mod}.sh "./runscript_${mod}.sh"
-    cp -rp "${HOME}"/gitdir/remoll/build/macros/runexample_${mod}.mac "./runexample_${mod}.mac"
+    cp -rp "${BUILD}"/geometry/*${mod}* "./geometry/"
+    cp -rp "${BUILD}"/geometry/materials.xml "./geometry/"
+    cp -rp "${BUILD}"/geometry/schema "./geometry/"
+    cp -rp "${BUILD}"/remoll "./remoll"
+    cp -rp "${BUILD}"/macros/runscript_${mod}.sh "./runscript_${mod}.sh"
+    cp -rp "${BUILD}"/macros/runexample_${mod}.mac "./runexample_${mod}.mac"
     if [ $changeInclude -eq 0 ];
       then
       changeIncludes $i $mod $min $max $final
     fi
-    qsub ../../macros/runscript_${mod}.sh
+    qsub ./runscript_${mod}.sh
     sleep 1
     cd ..
   done
@@ -63,8 +76,7 @@ EOM
 
 macroPrinter () {
 
-  buildLocation=$HOME/gitdir/remoll/build/
-  cd $buildLocation
+  cd $BUILD
 
   macroFileName="macros/runexample_${mod}.mac"
 
@@ -74,8 +86,8 @@ macroPrinter () {
 /remoll/setgeofile geometry/mollerMother_${mod}.gdml #../../geometry/mollerMother_${mod}.gdml
 # This must be explicitly called
 /run/initialize
-/remoll/addfield $buildLocation/map_directory/blockyHybrid_rm_3.0.txt
-/remoll/addfield $buildLocation/map_directory/blockyUpstream_rm_1.1.txt
+/remoll/addfield $BUILD/map_directory/blockyHybrid_rm_3.0.txt
+/remoll/addfield $BUILD/map_directory/blockyUpstream_rm_1.1.txt
 # Raster and initial angle stuff
 /remoll/oldras false
 /remoll/rasx 5 mm
@@ -99,8 +111,7 @@ EOM
 
 runscriptPrinter () {
 
-  buildLocation=$HOME/gitdir/remoll/build/
-  cd $buildLocation
+  cd $BUILD
 
   qsubFileName="macros/runscript_${mod}.sh"
   
@@ -119,92 +130,90 @@ EOM
 
 motherGDMLPrinter () {
 
-  buildLocation=$HOME/gitdir/remoll/build/
-  cd $buildLocation
+  cd $BUILD
 
   motherFileName="geometry/mollerMother_${mod}.gdml"
                                         # Be prepared to remove the ${mod}s from the superfluous sub files each time
                                         # Hard code the ones hat I know work for each application.
 /bin/cat <<EOM >$motherFileName
-  <?xml version="1.0" encoding="UTF-8" standalone="no" ?>
-  <gdml xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/gdml.xsd">
+<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<gdml xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/gdml.xsd">
 
-  <define> 
-    <position name="hallCenter" x="0" y="0" z="4000"/>
-    <position name="targetCenter" x="0" y="0" z="0"/>
-    <position name="upstreamCenter" x="0" y="0" z="7000."/>
-    <position name="hybridCenter" x="0" y="0" z="13366.57"/>
-    <position name="detectorCenter" x="0" y="0" z="28500."/>
-    <position name="dumpCenter" x="0" y="0" z="28500."/>
-    <rotation name="identity"/>
-  </define>
+<define> 
+  <position name="hallCenter" x="0" y="0" z="4000"/>
+  <position name="targetCenter" x="0" y="0" z="0"/>
+  <position name="upstreamCenter" x="0" y="0" z="7000."/>
+  <position name="hybridCenter" x="0" y="0" z="13366.57"/>
+  <position name="detectorCenter" x="0" y="0" z="28500."/>
+  <position name="dumpCenter" x="0" y="0" z="28500."/>
+  <rotation name="identity"/>
+</define>
 
-  <materials>
-       <material Z="1" name="Vacuum" state="gas">
-         <T unit="K" value="2.73"/>
-         <P unit="pascal" value="3e-18"/>
-         <D unit="g/cm3" value="1e-25"/>
-         <atom unit="g/mole" value="1.01"/>
-       </material>
-  </materials>
+<materials>
+     <material Z="1" name="Vacuum" state="gas">
+       <T unit="K" value="2.73"/>
+       <P unit="pascal" value="3e-18"/>
+       <D unit="g/cm3" value="1e-25"/>
+       <atom unit="g/mole" value="1.01"/>
+     </material>
+</materials>
 
-  <solids>
-       <box lunit="mm" name="boxMother" x="200000" y="200000" z="200000"/>
-  </solids>
+<solids>
+     <box lunit="mm" name="boxMother" x="200000" y="200000" z="200000"/>
+</solids>
 
-    <structure>
+  <structure>
 
-      <volume name="logicMother">
-        <materialref ref="Vacuum"/>
-        <solidref ref="boxMother"/>
+    <volume name="logicMother">
+      <materialref ref="Vacuum"/>
+      <solidref ref="boxMother"/>
 
-        <physvol>
-        <file name="geometry/targetDaughter_${mod}.gdml"/>
-        <positionref ref="targetCenter"/>
-        <rotationref ref="identity"/>
-        </physvol>
+      <physvol>
+      <file name="geometry/targetDaughter_${mod}.gdml"/>
+      <positionref ref="targetCenter"/>
+      <rotationref ref="identity"/>
+      </physvol>
 
+      <physvol>
+      <file name="geometry/hallDaughter_${mod}.gdml"/>
+      <positionref ref="hallCenter"/>
+      <rotationref ref="identity"/>
+      </physvol>
 
-        <physvol>
-        <file name="geometry/hallDaughter_${mod}.gdml"/>
-        <positionref ref="hallCenter"/>
-        <rotationref ref="identity"/>
-        </physvol>
+      <physvol>
+      <file name="geometry/detectorDaughter_${mod}.gdml"/>
+      <positionref ref="detectorCenter"/>
+      <rotationref ref="identity"/>
+      </physvol>
 
-        <physvol>
-        <file name="geometry/detectorDaughter_${mod}.gdml"/>
-        <positionref ref="detectorCenter"/>
-        <rotationref ref="identity"/>
-        </physvol>
+      <physvol>
+      <file name="geometry/upstreamDaughter_${mod}.gdml"/>
+      <positionref ref="upstreamCenter"/>
+      <rotationref ref="identity"/>
+      </physvol>
 
-        <physvol>
-        <file name="geometry/upstreamDaughter_${mod}.gdml"/>
-        <positionref ref="upstreamCenter"/>
-        <rotationref ref="identity"/>
-        </physvol>
+      <physvol>
+      <file name="geometry/hybridDaughter_${mod}.gdml"/>
+      <positionref ref="hybridCenter"/>
+      <rotationref ref="identity"/>
+      </physvol>    
 
-        <physvol>
-        <file name="geometry/hybridDaughter_${mod}.gdml"/>
-        <positionref ref="hybridCenter"/>
-        <rotationref ref="identity"/>
-        </physvol>    
+<!--
+      <physvol>
+      <file name="geometry/dumpDaughter_${mod}.gdml"/>
+      <positionref ref="hybridCenter"/>
+      <rotationref ref="identity"/>
+      </physvol>    
+-->
 
-  <!--
-        <physvol>
-        <file name="geometry/dumpDaughter_${mod}.gdml"/>
-        <positionref ref="hybridCenter"/>
-        <rotationref ref="identity"/>
-        </physvol>    
-  -->
+    </volume>
+  </structure>
 
-      </volume>
-    </structure>
-
-    <setup name="Default" version="1.0">
-      <world ref="logicMother"/>
-    </setup>
-   
-  </gdml> 
+  <setup name="Default" version="1.0">
+    <world ref="logicMother"/>
+  </setup>
+ 
+</gdml> 
 EOM
 }
 
