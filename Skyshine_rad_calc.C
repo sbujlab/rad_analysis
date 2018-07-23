d394c7d12d4511ac040abb6f6309ff25acc5aa49
Cameron Clarke <cameronc137@gmail.com>
2018-07-23 11:22:37 -0400

    Updating some descriptive code and defaults for remoll skyshine hallRad code

--- Skyshine_rad_calc.C
+++ Skyshine_rad_calc.C
@@ -1,7 +1,7 @@
 using namespace std;
 #include <iostream>
 
-void Skyshine_rad_calc(string modifier = "thickness"){//,string units = "mm",int number = 6){
+void Skyshine_rad_calc(string modifier = "thickness", const int nEvents = 1000000){//,string units = "mm",int number = 6){
 ////to use, either run the parser_hallrad_root.sh with the modifier passed in, or use the 'SAMs' modifier with "benchmark" and any number/no number as the third entry
 
 
@@ -23,8 +23,8 @@ void Skyshine_rad_calc(string modifier = "thickness"){//,string units = "mm",int
 //use parserHallrad format to loop over files instead of doing it here
   TFile *file_in;
   string ben = "benchmark";
-  int nEvents = 1e5;//9e6 for regular runs, 54e6 for benchmark runs;
-  file_in = TFile::Open(Form("%s_hallRad.root",modifier.c_str()));
+  //int nEvents = 1e5;//9e6 for regular runs, 54e6 for benchmark runs;
+  file_in = TFile::Open(Form("%s",modifier.c_str()));
   
   //TH1D *hist_101_g_neilLogX=(TH1D*)file_in->Get("Det_101/ha_101_g_neilLogX");
   //TH1D *hist_101_e_neilLogX=(TH1D*)file_in->Get("Det_101/ha_101_e_neilLogX");
@@ -40,30 +40,30 @@ void Skyshine_rad_calc(string modifier = "thickness"){//,string units = "mm",int
   Double_t NEIL_101 = hist_hSummary_neilLogX->GetBinContent(hist_hSummary_neilLogX->GetXaxis()->FindBin("101 Avg"));
   Double_t NEIL_101_error = hist_hSummary_neilLogX->GetBinError(hist_hSummary_neilLogX->GetXaxis()->FindBin("101 Avg"));
   Double_t Flux_101 = hist_101_n_enerLogX->Integral(hist_101_n_enerLogX->GetXaxis()->FindBin(25.12),94);
-  Double_t Flux_101_error = sqrt(Flux_101/nEvents);//9M events is the number per run that the averages normalize to
+  Double_t Flux_101_error = sqrt(Flux_101/nEvents);
   Double_t Energy_101 = hist_hSummary_enerLogX->GetBinContent(hist_hSummary_enerLogX->GetXaxis()->FindBin("101 Avg"));
   Double_t Energy_101_error = hist_hSummary_enerLogX->GetBinError(hist_hSummary_enerLogX->GetXaxis()->FindBin("101 Avg"));
 
 /*SAMs benchmark
-  Absolute per event: Neil in LHRS 1001 = 7.98787e-05(9.111e-07), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 2.03071e-05(1.50211e-06), Energy in 3201 = 0.108771(6.84946e-05)
-  Relative per event: Neil in LHRS 1001 = 1(0.0161306), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 1(0.0798966), Energy 3201 = 1(0.00089055)
+  Absolute per event: Neil in LHRS 1001 = 7.98787e-05(9.111e-07), Flux of neutrons to 1006, per event (E>25.12 MeV) = 2.03071e-05(1.50211e-06), Energy in 3201 = 0.108771(6.84946e-05)
+  Relative per event: Neil in LHRS 1001 = 1(0.0161306), Flux of neutrons to 1006, per event (E>25.12 MeV) = 1(0.0798966), Energy 3201 = 1(0.00089055)
   noSAMs benchmark
-  Absolute per event: Neil in LHRS 1001 = 2.27186e-05(6.09897e-07), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 1.30612e-05(1.20468e-06), Energy in 3201 = 0.0113672(1.73374e-05)
-  Relative per event: Neil in LHRS 1001 = 0.284414(0.00829588), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 0.643185(0.0624217), Energy 3201 = 0.104506(0.000172445)
+  Absolute per event: Neil in LHRS 1001 = 2.27186e-05(6.09897e-07), Flux of neutrons to 1006, per event (E>25.12 MeV) = 1.30612e-05(1.20468e-06), Energy in 3201 = 0.0113672(1.73374e-05)
+  Relative per event: Neil in LHRS 1001 = 0.284414(0.00829588), Flux of neutrons to 1006, per event (E>25.12 MeV) = 0.643185(0.0624217), Energy 3201 = 0.104506(0.000172445)
   SAMs_Ushield benchmark
-  Absolute per event: Neil in LHRS 1001 = 6.40978e-05(7.04657e-07), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 2.02412e-05(1.49967e-06), Energy in 3201 = 0.107989(6.49807e-05)
-  Relative per event: Neil in LHRS 1001 = 0.80244(0.0127119), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 0.996754(0.0797483), Energy 3201 = 0.992815(0.00086473)
+  Absolute per event: Neil in LHRS 1001 = 6.40978e-05(7.04657e-07), Flux of neutrons to 1006, per event (E>25.12 MeV) = 2.02412e-05(1.49967e-06), Energy in 3201 = 0.107989(6.49807e-05)
+  Relative per event: Neil in LHRS 1001 = 0.80244(0.0127119), Flux of neutrons to 1006, per event (E>25.12 MeV) = 0.996754(0.0797483), Energy 3201 = 0.992815(0.00086473)
   noSAMs_Ushield benchmark
-  Absolute per event: Neil in LHRS 1001 = 1.05303e-05(3.83341e-07), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 1.27087e-05(1.18831e-06), Energy in 3201 = 0.0112639(1.69927e-05)
-  Relative per event: Neil in LHRS 1001 = 0.131829(0.00502909), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 0.625826(0.0614931), Energy 3201 = 0.103556(0.000169288)
+  Absolute per event: Neil in LHRS 1001 = 1.05303e-05(3.83341e-07), Flux of neutrons to 1006, per event (E>25.12 MeV) = 1.27087e-05(1.18831e-06), Energy in 3201 = 0.0112639(1.69927e-05)
+  Relative per event: Neil in LHRS 1001 = 0.131829(0.00502909), Flux of neutrons to 1006, per event (E>25.12 MeV) = 0.625826(0.0614931), Energy 3201 = 0.103556(0.000169288)
   improvedSAMs_Ushield benchmark
-  Absolute per event: Neil in LHRS 1001 = 1.71573e-05(4.60409e-07), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 1.28942e-05(1.19695e-06), Energy in 3201 = 0.023519(2.84886e-05)
-  Relative per event: Neil in LHRS 1001 = 0.214791(0.00626292), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 0.634963(0.061983), Energy 3201 = 0.216225(0.000295192)
+  Absolute per event: Neil in LHRS 1001 = 1.71573e-05(4.60409e-07), Flux of neutrons to 1006, per event (E>25.12 MeV) = 1.28942e-05(1.19695e-06), Energy in 3201 = 0.023519(2.84886e-05)
+  Relative per event: Neil in LHRS 1001 = 0.214791(0.00626292), Flux of neutrons to 1006, per event (E>25.12 MeV) = 0.634963(0.061983), Energy 3201 = 0.216225(0.000295192)
 
   prexSim 1006 == remoll 101
 
 */
-  //Absolute per event: Neil in LHRS 1001 = 1.0121e-05(1.28749e-07), Flux of neutrons to 1006, per million events (E>25.12 MeV) = 1.27963e-05(1.6863e-07), Energy in 3201 = 0.0113008(5.94492e-06)
+  //Absolute per event: Neil in LHRS 1001 = 1.0121e-05(1.28749e-07), Flux of neutrons to 1006, per event (E>25.12 MeV) = 1.27963e-05(1.6863e-07), Energy in 3201 = 0.0113008(5.94492e-06)
   Double_t bench_NEIL_101 = 4.9331e-4;
   Double_t bench_NEIL_101_error = 9.51624e-07;  // error propagation = sigma = value after modification * relative error of subparts added in quadratures (sigma/previous value)
   Double_t bench_Flux_101 = 1.27963e-05;
@@ -95,10 +95,10 @@ void Skyshine_rad_calc(string modifier = "thickness"){//,string units = "mm",int
 
   // This simulation analysis assumes a modifier describing a kind of benchmark wih 54 million events, or a varied parameter of units and number with 9 million events, and everything is normalized to NEIL, Flux, and Energy per 1M events on target
   if (units!=ben){ //case = doing parameter space searches
-    file_out<<"Modified = "<<modifier<<", units = "<<units<<", number = "<<number<<std::endl;
-    NEIL_101_out<<modifier<<","<<units<<","<<number<<","<<NEIL_101_ratio<<","<<NEIL_101_ratio_error<<std::endl;
-    Flux_101_out<<modifier<<","<<units<<","<<number<<","<<Flux_101_ratio<<","<<Flux_101_ratio_error<<std::endl;
-    Energy_101_out<<modifier<<","<<units<<","<<number<<","<<Energy_101_ratio<<","<<Energy_101_ratio_error<<std::endl;
+    file_out<<"Modified = "<<modifier<<std::endl;
+    NEIL_101_out<<modifier<<","<<NEIL_101_ratio<<","<<NEIL_101_ratio_error<<std::endl;
+    Flux_101_out<<modifier<<","<<Flux_101_ratio<<","<<Flux_101_ratio_error<<std::endl;
+    Energy_101_out<<modifier<<","<<Energy_101_ratio<<","<<Energy_101_ratio_error<<std::endl;
   }
   else{ //case = benchmark
     file_out<<"Modified = "<<modifier<<", benchmark"<<std::endl;
@@ -106,10 +106,10 @@ void Skyshine_rad_calc(string modifier = "thickness"){//,string units = "mm",int
     Flux_101_out<<modifier<<","<<ben<<","<<ben<<","<<Flux_101_ratio<<","<<Flux_101_ratio_error<<std::endl;
     Energy_101_out<<modifier<<","<<ben<<","<<ben<<","<<Energy_101_ratio<<","<<Energy_101_ratio_error<<std::endl;
   }
-  std::cout<<"Absolute per event: Neil in 101 = "<<NEIL_101<<"("<<NEIL_101_error<<"), Flux of neutrons to 101, per million events (E>25.12 MeV) = "<<Flux_101<<"("<<Flux_101_error<<"), Energy in 101 = "<<Energy_101<<"("<<Energy_101_error<<")"<<std::endl;
-  std::cout<<"Relative per event: Neil in 101 = "<<NEIL_101_ratio<<"("<<NEIL_101_ratio_error<<"), Flux of neutrons to 101, per million events (E>25.12 MeV) = "<<Flux_101_ratio<<"("<<Flux_101_ratio_error<<"), Energy in 101 = "<<Energy_101_ratio<<"("<<Energy_101_ratio_error<<")"<<std::endl;
-  file_out<<"Absolute per event: Neil in 101 = "<<NEIL_101<<", Flux of neutrons to 101, per million events (E>25.12 MeV) = "<<Flux_101<<", Energy in 101 = "<<Energy_101<<std::endl;
-  file_out<<"Relative per event: Neil in 101 = "<<NEIL_101_ratio<<", Flux of neutrons to 101, per million events (E>25.12 MeV) = "<<Flux_101_ratio<<", Energy in 101 = "<<Energy_101_ratio<<std::endl;
+  std::cout<<"Absolute per event: Neil in 101 = "<<NEIL_101<<"("<<NEIL_101_error<<"), Flux of neutrons to 101, per event (E>25.12 MeV) = "<<Flux_101<<"("<<Flux_101_error<<"), Energy in 101 = "<<Energy_101<<"("<<Energy_101_error<<")"<<std::endl;
+  std::cout<<"Relative per event: Neil in 101 = "<<NEIL_101_ratio<<"("<<NEIL_101_ratio_error<<"), Flux of neutrons to 101, per event (E>25.12 MeV) = "<<Flux_101_ratio<<"("<<Flux_101_ratio_error<<"), Energy in 101 = "<<Energy_101_ratio<<"("<<Energy_101_ratio_error<<")"<<std::endl;
+  file_out<<"Absolute per event: Neil in 101 = "<<NEIL_101<<", Flux of neutrons to 101, per event (E>25.12 MeV) = "<<Flux_101<<", Energy in 101 = "<<Energy_101<<std::endl;
+  file_out<<"Relative per event: Neil in 101 = "<<NEIL_101_ratio<<", Flux of neutrons to 101, per event (E>25.12 MeV) = "<<Flux_101_ratio<<", Energy in 101 = "<<Energy_101_ratio<<std::endl;
 //print the output and ratio wrt benchmark into the outfile (append)
   file_out.close();
  
