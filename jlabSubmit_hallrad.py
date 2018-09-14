4491067a93b3b4581804bcd3569ec8cda34085d0
Cameron Clarke <cameronc137@gmail.com>
2018-09-14 16:02:14 -0400

    Updated the hallrad jlabsubmit code to actually use rad_dose instead (hallRad wasn't behaving, but that may have been due to low statistics test run, anyway, rad_dose is better). And making jlabsubmit also run pruneTree on the output (saving a factor of 50,000 in file size)

--- jlabSubmit_hallrad.py
+++ jlabSubmit_hallrad.py
@@ -7,10 +7,9 @@ def main():
 #FIXME Update these
     email = "cameronc@jlab.org"
 
-    stage = raw_input("Please enter the stage (merged, etc.): ")
+    stage = raw_input("Please enter the indicative name (merged, block1_height_10cm, etc.): ")
     number = raw_input("Please enter the number (100M, etc.): ")
     identifier = "moller_"+stage+"_"+number#raw_input("Please enter the identifier: ")
-    parameter = "null"#raw_input("Please enter the parameter: ")
 
     #sourceDir = "/work/halla/parity/disk1/ciprian/prexSim"
     sourceDir = "/work/halla/parity/disk1/moller12gev/cameronc/remoll"
@@ -20,21 +19,18 @@ def main():
         os.makedirs(outputDir)
 
     jobName=identifier
-    listName='list_' + identifier
 
     if not os.path.exists(outputDir+"/"+jobName+"/log"):
         os.makedirs(outputDir+"/"+jobName+"/log")
-    createXMLfile(sourceDir,outputDir,jobName,identifier,listName,parameter,stage,email)
+    createXMLfile(sourceDir,outputDir,jobName,identifier,stage,email)
 
-    call(["cp",sourceDir+"/rad_analysis/ls_mod.sh",
-              outputDir+"/"+jobName+"/ls_mod.sh"])
     call(["cp",sourceDir+"/rad_analysis/libremoll.so",
               outputDir+"/"+jobName+"/libremoll.so"])
     call(["cp",sourceMasterDir+"/rad_analysis/hallRad",
               outputDir+"/"+jobName+"/hallRad"])
-    print "All done for configuration ",identifier,"for parameter search: ",parameter
+    print "All done for configuration ",identifier
 
-def createXMLfile(source,writeDir,idRoot,name,listname,par,stag,email):
+def createXMLfile(source,writeDir,idRoot,name,stag,email):
 
     if not os.path.exists(source+"/output/jobs"):
         os.makedirs(source+"/output/jobs")
@@ -42,7 +38,7 @@ def createXMLfile(source,writeDir,idRoot,name,listname,par,stag,email):
     f=open(source+"/output/jobs/"+idRoot+"_hallRad.xml","w")
     f.write("<Request>\n")
     f.write("  <Email email=\""+email+"\" request=\"false\" job=\"true\"/>\n")
-    f.write("  <Project name=\"prex\"/>\n")
+    f.write("  <Project name=\"moller12gev\"/>\n")
 
     f.write("  <Track name=\"analysis\"/>\n")
 #    f.write("  <Track name=\"analysis\"/>\n")
@@ -53,18 +49,17 @@ def createXMLfile(source,writeDir,idRoot,name,listname,par,stag,email):
 
     f.write("  <Command><![CDATA[\n")
     f.write("    pwd\n")
-    f.write("    ./ls_mod.sh "+stag+"\n")
+    f.write("    ls ../../"+idRoot+"/*"+idRoot+"*/remollout.root >> list_"+idRoot+".txt\n")
+    f.write("    ./hallRad --infile list_"+idRoot+".txt --detList 101\n")
     
     f.write("  ]]></Command>\n")
 
     idName= writeDir+"/"+idRoot
     f.write("  <Job>\n")
     f.write("    <Input src=\""+idName+"/hallRad\" dest=\"hallRad\"/>\n")
-    f.write("    <Input src=\""+idName+"/ls_mod.sh\" dest=\"ls_mod.sh\"/>\n")
     f.write("    <Input src=\""+idName+"/libremoll.so\" dest=\"libremoll.so\"/>\n")
-    #f.write("    <Input src=\""+idName+"/"+listname+".txt\" dest=\""+listname+".txt\"/>\n")
-    f.write("    <Output src=\""+listname+".txt\" dest=\""+idName+"/"+listname+".txt\"/>\n")
-    f.write("    <Output src=\""+listname+"_hallRad.root\" dest=\""+idName+"/"+listname+"_hallRad.root\"/>\n")
+    f.write("    <Output src=\"list_"+idRoot+".txt\" dest=\""+idName+"/list_"+idRoot+".txt\"/>\n")
+    f.write("    <Output src=\"list_"+idRoot+"_hallRad.root\" dest=\""+idName+"/list_"+idRoot+"_hallRad.root\"/>\n")
 
     f.write("    <Stdout dest=\""+idName+"/log/log.out\"/>\n")
     f.write("    <Stderr dest=\""+idName+"/log/log.err\"/>\n")
