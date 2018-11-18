e341c891e1a8e08d557248b69b0816ccc493900d
Cameron Clarke <cameronc137@gmail.com>
2018-11-17 21:54:34 -0500

    Updated multi-plotter

--- plotMultConfigRatios.C
+++ plotMultConfigRatios.C
@@ -48,12 +48,11 @@ vector<vector<string>> CSVParse(TString fileName)
 }
 TCanvas * c1 = new TCanvas();
 TMultiGraph *mg = new TMultiGraph();
-bool alreadyDrew = false;
-void plotConfigRatiosHelper(string fileName = "NEIL_1001") {
-	string mode = "Modified"; // "Benchmark";//"Modified"
+int alreadyDrew = 0;
+void plotConfigRatiosHelper(string fileMode = "cadSAMs", string fileMod = "thin", string fileType = "NEIL_1001") {
 
 	// Format: thickness,mm,5,0.99041,0.0301189
-	vector<vector<string>> data = CSVParse(Form("output/SAM_analysis_%s.csv",fileName.c_str()));
+	vector<vector<string>> data = CSVParse(Form("output/SAM_%s_%s_%s.csv",fileMode.c_str(),fileMod.c_str(),fileType.c_str()));
 	int len = data.size();
 	// Plot data
 	//   for each entry in the vector, open the sub-vector and print the modifier to the plot name (with the units)
@@ -116,51 +115,67 @@ void plotConfigRatiosHelper(string fileName = "NEIL_1001") {
 	double numberMin = sortedNumber[0];
 	double numberMax = sortedNumber[len-1-nBen];
 
-	if (mode=="Benchmark"){
+	if (fileMode=="benchmark"){
 		TGraphErrors * graphBen = new TGraphErrors(nBen,numberBen,valueBen,0,errorBen);
-		graphBen->SetTitle(Form("Plot of %s %s configurations",fileName.c_str(),newModifiers[0].c_str()));
+		graphBen->SetTitle(Form("Plot of %s %s %s configurations",fileMod.c_str(),fileType.c_str(),newModifiers[0].c_str()));
 		graphBen->SetMarkerColor(4);
 		graphBen->SetMarkerStyle(21);
 		graphBen->GetXaxis()->SetTitle(Form("Benchmarks"));
-		graphBen->GetYaxis()->SetTitle(Form("Relative %s",fileName.c_str()));
+		graphBen->GetYaxis()->SetTitle(Form("Relative %s %s",fileMod.c_str(),fileType.c_str()));
 		//graph->SetMarkerSize(.4);
-		if (alreadyDrew)
+		if (alreadyDrew>0)
 		{
-			graphBen->SetMarkerColor(2);
+			graphBen->SetMarkerColor(alreadyDrew);
 			graphBen->SetMarkerStyle(21);
 			mg->Add(graphBen);
 			mg->Draw("APEsame");
+			alreadyDrew++;
 		}
 		else{
 			mg->Add(graphBen);
 			mg->Draw("APE");
-			alreadyDrew=true;
+			alreadyDrew++;
 		}
 		c1->Update();
-		c1->SaveAs(Form("Plot_%s_%s.pdf",modifiersBen[0].c_str(),fileName.c_str()));
+		c1->SaveAs(Form("Plot_%s_%s_%s.pdf",modifiersBen[0].c_str(),fileMod.c_str(),fileType.c_str()));
 	}
 	else{
 		TGraphErrors * graph = new TGraphErrors(len-nBen,newNumber,newValue,0,newError);
-		graph->SetTitle(Form("Plot of %s %s configurations, size %.1f to %.1f %s",fileName.c_str(),newModifiers[0].c_str(),numberMin,numberMax,newUnits[0].c_str()));
-		graph->SetMarkerColor(4);
+		//graph->SetTitle(Form("Plot of %s %s configurations, size %.1f to %.1f %s",fileName.c_str(),newModifiers[0].c_str(),numberMin,numberMax,newUnits[0].c_str()));
+		graph->SetTitle(Form("%s %s",fileMod.c_str(),fileType.c_str()));
+		graph->SetMarkerColor(alreadyDrew+1);
 		graph->SetMarkerStyle(21);
-		graph->GetXaxis()->SetTitle(Form("%s %s",newModifiers[0].c_str(),newUnits[0].c_str()));
-		graph->GetYaxis()->SetTitle(Form("Relative %s",fileName.c_str()));
+        graph->Fit("pol1");
+        graph->SetLineColor(alreadyDrew+1);
+        graph->GetFunction("pol1")->SetLineColor(alreadyDrew+1);
+        gStyle->SetOptFit(0111);
+        gStyle->SetEndErrorSize(6);
 		//graph->SetMarkerSize(.4);
-		if (alreadyDrew)
-		{
-			graph->SetMarkerColor(2);
-			graph->SetMarkerStyle(21);
-			mg->Add(graph);
-			mg->Draw("APEsame");
-		}
-		else{
-			mg->Add(graph);
-			mg->Draw("APE");
-			alreadyDrew=true;
-		}
+		mg->Add(graph);
+		mg->Draw("APE");
+        mg->GetXaxis()->SetTitle(Form("%s",newUnits[0].c_str()));//" %s (%s)",newModifiers[0].c_str(),newUnits[0].c_str()));
+        mg->GetYaxis()->SetTitle(Form("%s          ",newModifiers[0].c_str()));
+	    //mg->GetXaxis()->SetTitle(Form("cylindrical can thickness (mils)"));//" %s (%s)",newModifiers[0].c_str(),newUnits[0].c_str()));
+	    //mg->GetYaxis()->SetTitle("Relative to no SAMs, new E, new target position     ");
+		alreadyDrew++;
+        int n = graph->GetN();
+        double* y = graph->GetY();
+        int locmax = TMath::LocMax(n,y);
+        double tmax = 1.15*y[locmax];
+        int locmin = TMath::LocMin(n,y);
+        double tmin = 0.80*y[locmin];
+        mg->SetMinimum(tmin);
+        mg->SetMaximum(tmax);
+        c1->BuildLegend(0.125,0.8,0.5,0.9);
 		c1->Update();
-		c1->SaveAs(Form("Plot_%s_%s.pdf",modifiers[4].c_str(),fileName.c_str()));
+        TPaveStats * stats = (TPaveStats*)graph->GetListOfFunctions()->FindObject("stats");
+        stats->SetTextColor(alreadyDrew);
+        stats->SetX1NDC(0.333*(alreadyDrew-1)); 
+        stats->SetX2NDC(0.333*alreadyDrew);
+        stats->SetY1NDC(0.9);
+        stats->SetY2NDC(1.0);
+		c1->Update();
+		c1->SaveAs(Form("Plot_%s_%s.pdf",fileMode.c_str(),fileType.c_str()));
 	}
 	delete[] modifiers;
 	delete[] units;
@@ -172,9 +187,23 @@ void plotConfigRatiosHelper(string fileName = "NEIL_1001") {
 	delete[] errorBen;
 }
 
-void plotConfigRatios(int numLines = 1, string fileName1 = "NEIL_1001", string fileName2 = "NEIL_1001") {
-	plotConfigRatiosHelper(fileName1);
-	if (numLines>1){
-		plotConfigRatiosHelper(fileName2);
-	}
+void plotMultConfigRatios(int numLines = 0, string mode = "cadSAMs", string type = "NEIL_1001", string fileMod1 = "thin", string fileMod2 = "thick", string fileMod3 = "allthick", string fileMod4 = "allthick20", string fileMod5 = "allthick30") {
+    if (numLines==0){
+        printf("USAGE: .x plotMultConfigRatios(int number of files,\"mode (cadSAMs, etc...)\",\"config type (NEIL_1001)\",\"modifier (thick, thin, etc...)\"\n");
+    }
+    else{
+        plotConfigRatiosHelper(mode, fileMod1,type);
+        if (numLines>1){
+            plotConfigRatiosHelper(mode, fileMod2,type);
+        }
+        if (numLines>2){
+            plotConfigRatiosHelper(mode, fileMod3,type);
+        }
+        if (numLines>3){
+            plotConfigRatiosHelper(mode, fileMod4,type);
+        }
+        if (numLines>4){
+            plotConfigRatiosHelper(mode, fileMod5,type);
+        }
+    }
 }
